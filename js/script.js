2c673c716fff7663dee1d8daf548404d9e55d935
Ric Bevan <bevoid@icloud.com>
2017-10-20 10:20:34 +0100

    Highlight relevant opening times

--- js/script.js
+++ js/script.js
@@ -19,7 +19,42 @@ $( document ).ready(function() {
       img.removeAttribute('data-src');
     };
   });
+
+  highlightOpeningTimes();
 });
+
+function highlightOpeningTimes() {
+  var today = new Date();
+  var day = today.getDay();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1;
+  var yyyy = today.getFullYear();
+
+  today = dd + '/' + mm + '/' + yyyy;
+
+  var bankHolidays = [
+    '25/12/2017',
+    '26/12/2017',
+    '1/1/2017',
+    '30/3/2017',
+    '2/4/2017',
+    '7/5/2017',
+    '28/5/2017',
+    '27/8/2017',
+    '25/12/2017',
+    '26/12/2017'
+  ];
+
+  if (bankHolidays.indexOf(today) >= 1)
+    day = 0;
+
+  if (day == 5)
+    $('#fri-opening-times').addClass('uk-text-bold');
+  else if ((day >= 1) && (day <= 5))
+    $('#mon-thur-opening-times').addClass('uk-text-bold');
+  else
+    $('#sat-sun-opening-times').addClass('uk-text-bold');
+}
 
 function filterColour(colour) {
   var colourGrid = $('#colour-grid');
